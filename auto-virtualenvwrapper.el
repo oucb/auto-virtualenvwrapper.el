f0efdde1683e823433b99a25e9c3333edde0be4f
oucb <oucb@outlook.com>
2019-12-03 16:38:32 +0800

    refactor(auto-virtualenvwrapper.el): add 'pyproject.toml' of poetry for project filter

--- auto-virtualenvwrapper.el
+++ auto-virtualenvwrapper.el
@@ -64,7 +64,7 @@ any virtualenv. This is nil by default, for backward compatibility."
   :group 'auto-virtualenvwrapper)
 
 (defvar auto-virtualenvwrapper-project-root-files
-  '(".python-version" ".dir-locals.el" ".projectile" ".emacs-project" ".workon" "Pipfile" "manage.py")
+  '(".python-version" ".dir-locals.el" ".projectile" ".emacs-project" ".workon" "Pipfile" "pyproject.toml" "manage.py")
   "The presence of any file/directory in this list indicates a project root.")
 
 (defvar auto-virtualenvwrapper-verbose t
